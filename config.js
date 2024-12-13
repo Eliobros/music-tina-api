57f784e9af7b6438168aef4614d4336f17ef9637
Luisa Dev <eliobrostech3@gmail.com>
2024-12-13 16:59:36 +0200

    Alterando o config

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ exports.NAME_API = "Tina Music API";
 
 exports.VERSION_API = "1.0.0"
 
-exports.DIR_GENERATE_KEY = path.resolve(__dirname, "./public");
+exports.DIR_GENERATE_KEY = path.resolve(__dirname, "public", "generate-api-key.html");
 
 exports.INFO_USE = "Bom Essan API funciona da seguinte maneira voce faz a requisicao o usuando o nosso endpoint com a rota  [api/music] voce podera fazer requisicao pedindo por exemplo uma musica voce simplismente diz o nome da musica junto de um comando na qual voce ira criar em seu bot mas lembre se que para funcionar voce devera obetr a chave de api da TINA HOST no site [https://tina-host.netlify.app] bom e quase isso espoero que nao tenhas nenhuma duvida."
 
